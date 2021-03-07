5d215b0a299a9a1c96b38957a859eb238acd2306
Mayank Rikh <mayankrikh@gmail.com>
2021-03-07 13:06:28 +0000

    Initial Commit

---
+++ Health-Care-Rsearch-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Health-Care-Rsearch-SwiftUI
+//
+//  Created by Mayank Rikh on 07/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Health-Care-Rsearch-SwiftUI/Health_Care_Rsearch_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Health_Care_Rsearch_SwiftUIApp.swift
+//  Health-Care-Rsearch-SwiftUI
+//
+//  Created by Mayank Rikh on 07/03/21.
+//
+
+import SwiftUI
+
+@main
+struct Health_Care_Rsearch_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
